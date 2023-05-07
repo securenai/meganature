4f07e643971c5c56190a4c4e6d07f5a24148f74a
Michael Cheng <michaelnccu@gmail.com>
2023-05-07 13:44:13 +0800

    add contact form

--- data/contact/default.md
+++ data/contact/default.md
@@ -1,14 +1,16 @@
 ---
 name: MegaNature
 avatar: /static/images/mn.png
-occupation: 健康呼吸 健康生活123
-company: info@meganaturetech.com
-email: info@meganaturetech.com
-twitter: https://twitter.com/Twitter
-linkedin: https://www.linkedin.com
-github: https://github.com
+occupation: 健康呼吸 健康生活
+# company: info@meganaturetech.com
+# email: info@meganaturetech.com
+# twitter: https://twitter.com/Twitter
+# linkedin: https://www.linkedin.com
+# github: https://github.com
 ---
 
-請在此處撰寫公司聯絡方式...
+地址 : 新北市林口區四維路110號1樓之2
 
-關於恆自然聯絡方式...
+信箱 : info@meganature.com
+
+電話 : 02 2748 2285
--- layouts/ContactLayout.js
+++ layouts/ContactLayout.js
@@ -6,6 +6,10 @@ import Trail from '@/components/Effects/Trail'
 export default function ContactLayout({ children, frontMatter }) {
   const { name, avatar, occupation, company, email, twitter, linkedin, github } = frontMatter
 
+  function handleSubmit(event) {
+    event.preventDefault()
+  }
+
   return (
     <>
       <PageSEO title={`About - ${name}`} description={`About - ${name}`} />
@@ -16,13 +20,13 @@ export default function ContactLayout({ children, frontMatter }) {
           </h1>
         </div>
         <div className="items-start space-y-2 xl:grid xl:grid-cols-3 xl:gap-x-8 xl:space-y-0">
-          {/* <div className="mt-8 flex flex-col items-center bg-yellow-100 py-8">
+          {/* <div className="mt-8 flex flex-col items-center bg-green-200 py-8">
             <Image
               src={avatar}
-              alt="avatar"
+              alt="company"
               width="192px"
               height="192px"
-              className="h-48 w-48 rounded-full"
+              // className="h-48 w-48 rounded-full"
             />
             <h3 className="pt-4 pb-2 text-2xl font-bold leading-8 tracking-tight">{name}</h3>
             <div className="text-gray-500 dark:text-gray-400">{occupation}</div>
@@ -38,6 +42,80 @@ export default function ContactLayout({ children, frontMatter }) {
           <Trail open>
             <div className="prose max-w-none pt-8 pb-8 dark:prose-dark xl:col-span-2">
               {children}
+              <div>
+                <form
+                  className="w-full max-w-lg"
+                  onSubmit={handleSubmit}
+                  name="contact"
+                  data-netlify="true"
+                >
+                  <input type="hidden" name="form-name" value="contact" />
+
+                  <div className="mb-6 flex flex-wrap">
+                    <label className="mb-1 block w-full" htmlFor="name">
+                      Name:
+                    </label>
+                    <input
+                      className="w-full appearance-none rounded border border-gray-400 bg-white px-4 py-2 leading-tight text-gray-700 focus:border-blue-500 focus:bg-white focus:outline-none"
+                      id="name"
+                      type="text"
+                      name="name"
+                      placeholder="Your name"
+                      required
+                    />
+                  </div>
+
+                  <div className="mb-6 flex flex-wrap">
+                    <label className="mb-1 block w-full" htmlFor="phone">
+                      Phone:
+                    </label>
+                    <input
+                      className="w-full appearance-none rounded border border-gray-400 bg-white px-4 py-2 leading-tight text-gray-700 focus:border-blue-500 focus:bg-white focus:outline-none"
+                      id="phone"
+                      type="tel"
+                      name="phone"
+                      placeholder="Your phone number"
+                      required
+                    />
+                  </div>
+
+                  <div className="mb-6 flex flex-wrap">
+                    <label className="mb-1 block w-full" htmlFor="email">
+                      Email:
+                    </label>
+                    <input
+                      className="w-full appearance-none rounded border border-gray-400 bg-white px-4 py-2 leading-tight text-gray-700 focus:border-blue-500 focus:bg-white focus:outline-none"
+                      id="email"
+                      type="email"
+                      name="email"
+                      placeholder="Your email address"
+                      required
+                    />
+                  </div>
+
+                  <div className="mb-6 flex flex-wrap">
+                    <label className="mb-1 block w-full" htmlFor="message">
+                      Message:
+                    </label>
+                    <textarea
+                      className="h-48 w-full appearance-none rounded border border-gray-400 bg-white px-4 py-2 leading-tight text-gray-700 focus:border-blue-500 focus:bg-white focus:outline-none"
+                      id="message"
+                      name="message"
+                      placeholder="Write your message here"
+                      required
+                    ></textarea>
+                  </div>
+
+                  <div className="flex items-center justify-between">
+                    <button
+                      className="focus:shadow-outline rounded bg-blue-500 px-4 py-2 font-bold text-white hover:bg-blue-700 focus:outline-none"
+                      type="submit"
+                    >
+                      Send Message
+                    </button>
+                  </div>
+                </form>
+              </div>
             </div>
           </Trail>
         </div>
